6d75252a87368b34cdb7014bb0d450bf60c3addc
suynnn <suy00065@gmail.com>
2024-10-18 09:13:45 +0900

    Init: 프로젝트 초기 설정

---
+++ src/main/java/org/streaming/revenuemanagement/RevenueManagementApplication.java
@@ -0,0 +1,13 @@
+package org.streaming.revenuemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevenueManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevenueManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/streaming/revenuemanagement/RevenueManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.streaming.revenuemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevenueManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
