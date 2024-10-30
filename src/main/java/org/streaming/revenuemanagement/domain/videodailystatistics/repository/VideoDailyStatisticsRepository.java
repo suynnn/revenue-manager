3ede20e057f654d87e57f87540ab26751b7e9356
suynnn <suy00065@gmail.com>
2024-10-30 17:56:05 +0900

    Feat: 통계 데이터 조회 및 생성 기능 개발 완료

--- src/main/java/org/streaming/revenuemanagement/domain/videodailystatistics/repository/VideoDailyStatisticsRepository.java
+++ src/main/java/org/streaming/revenuemanagement/domain/videodailystatistics/repository/VideoDailyStatisticsRepository.java
@@ -3,10 +3,14 @@ package org.streaming.revenuemanagement.domain.videodailystatistics.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.streaming.revenuemanagement.domain.videodailystatistics.dto.VideoStatisticDto;
 import org.streaming.revenuemanagement.domain.videodailystatistics.entity.VideoDailyStatistics;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +19,26 @@ public interface VideoDailyStatisticsRepository extends JpaRepository<VideoDaily
     Optional<VideoDailyStatistics> findByVideoId(Long videoId);
 
     Page<VideoDailyStatistics> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+    // 조회수가 높은 TOP 5 동영상 가져오기
+    @Query("SELECT new org.streaming.revenuemanagement.domain.videodailystatistics.dto.VideoStatisticDto(v.videoId, SUM(v.dailyViews)) " +
+            "FROM VideoDailyStatistics v " +
+            "WHERE v.createdAt BETWEEN :start AND :end " +
+            "GROUP BY v.videoId " +
+            "ORDER BY SUM(v.dailyViews) DESC")
+    List<VideoStatisticDto> findTop5ByViews(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            Pageable pageable);
+
+    // 재생 시간이 긴 TOP 5 동영상 가져오기
+    @Query("SELECT new org.streaming.revenuemanagement.domain.videodailystatistics.dto.VideoStatisticDto(v.videoId, SUM(v.dailyPlayTime)) " +
+            "FROM VideoDailyStatistics v " +
+            "WHERE v.createdAt BETWEEN :start AND :end " +
+            "GROUP BY v.videoId " +
+            "ORDER BY SUM(v.dailyPlayTime) DESC")
+    List<VideoStatisticDto> findTop5ByPlayTime(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            Pageable pageable);
 }
