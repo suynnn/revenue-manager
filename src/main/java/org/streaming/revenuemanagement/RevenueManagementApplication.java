0312ef98d7cf71a95b1c55e5bb1297b61e565e10
suynnn <suy00065@gmail.com>
2024-11-04 19:10:02 +0900

    Fix: 컴포넌트 스캔으로 멀티레포들까지 스캔하도록 설정

--- src/main/java/org/streaming/revenuemanagement/RevenueManagementApplication.java
+++ src/main/java/org/streaming/revenuemanagement/RevenueManagementApplication.java
@@ -2,8 +2,11 @@ package org.streaming.revenuemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.revenue.management.batch", "org.revenue.management.common",
+        "org.revenue.management.member", "org.revenue.management.streaming"})
 public class RevenueManagementApplication {
 
     public static void main(String[] args) {
