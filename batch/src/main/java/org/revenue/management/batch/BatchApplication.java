3810d38bc203fe46878366a0cd5ed9f48b7a6760
suynnn <suy00065@gmail.com>
2024-11-05 16:22:11 +0900

    Config: 멀티모듈 yml 설정 및 componentscan 등록

--- batch/src/main/java/org/revenue/management/batch/BatchApplication.java
+++ batch/src/main/java/org/revenue/management/batch/BatchApplication.java
@@ -2,8 +2,11 @@ package org.revenue.management.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.revenue.management.common",
+        "org.revenue.management.member", "org.revenue.management.streaming"})
 public class BatchApplication {
 
     public static void main(String[] args) {
--- streaming/src/main/java/org/revenue/management/streaming/StreamingApplication.java
+++ streaming/src/main/java/org/revenue/management/streaming/StreamingApplication.java
@@ -2,8 +2,11 @@ package org.revenue.management.streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.revenue.management.batch", "org.revenue.management.common",
+        "org.revenue.management.member"})
 public class StreamingApplication {
 
     public static void main(String[] args) {
