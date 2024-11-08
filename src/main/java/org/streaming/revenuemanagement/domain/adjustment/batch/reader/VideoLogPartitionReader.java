dac53bbc8de5e86c52ec7a0ffa3eaa5462e3c733
suynnn <suy00065@gmail.com>
2024-11-09 03:01:49 +0900

    Review: 코드리뷰
    - 배치 코드의 reader, processor, writer 각각 분리하였습니다
    - 배치 파티셔닝 적용하였습니다
    - 파티셔닝 적용 후, 실행시간이 50% 넘게 감소하였습니다.
    - 기존에는 videoId를 기준으로 모든 videoStatistics들을 가져와 videoLog 데이터를 count 및 sum 연산 써서 데이터 합산해서 업데이트 해주었었습니다. 파티셔닝 적용하기 위해 videoLogId를 기준으로 모든 로그 데이터들을 가져와서 연산하게 해주었습니다.
    - 기존 방식으로는 파티셔닝이 제대로 적용되지 않았습니다.

---
+++ src/main/java/org/streaming/revenuemanagement/domain/adjustment/batch/reader/VideoLogPartitionReader.java
@@ -0,0 +1,41 @@
+package org.streaming.revenuemanagement.domain.adjustment.batch.reader;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+import org.streaming.revenuemanagement.domain.videolog.entity.VideoLog;
+import org.streaming.revenuemanagement.domain.videolog.repository.VideoLogRepository;
+
+import java.util.Arrays;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class VideoLogPartitionReader {
+
+    private final VideoLogRepository videoLogRepository;
+
+    @Bean
+    @StepScope
+    public RepositoryItemReader<VideoLog> videoLogPartitionReaderMethod(
+            @Value("#{stepExecutionContext['minId']}") Long minId,
+            @Value("#{stepExecutionContext['maxId']}") Long maxId,
+            @Value("${spring.batch.chunk.size}") Integer chunkSize) {
+
+        return new RepositoryItemReaderBuilder<VideoLog>()
+                .name("videoLogPartitionReader")
+                .repository(videoLogRepository)
+                .methodName("findVideoLogsByVideoIdRangeAndDateRange")
+                .arguments(Arrays.asList(minId, maxId))
+                .sorts(Map.of("id", Sort.Direction.ASC)) // 여기에 정렬 조건 추가
+                .pageSize(chunkSize)
+                .build();
+    }
+}
---
+++ src/main/java/org/streaming/revenuemanagement/domain/adjustment/batch/writer/VideoDailyStatisticsWriter.java
@@ -0,0 +1,20 @@
+package org.streaming.revenuemanagement.domain.adjustment.batch.writer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+import org.streaming.revenuemanagement.domain.videodailystatistics.entity.VideoDailyStatistics;
+import org.streaming.revenuemanagement.domain.videodailystatistics.repository.VideoDailyStatisticsRepository;
+
+@Component
+@RequiredArgsConstructor
+public class VideoDailyStatisticsWriter implements ItemWriter<VideoDailyStatistics> {
+
+    private final VideoDailyStatisticsRepository videoDailyStatisticsRepository;
+
+    @Override
+    public void write(Chunk<? extends VideoDailyStatistics> chunk) throws Exception {
+        videoDailyStatisticsRepository.saveAll(chunk);
+    }
+}
